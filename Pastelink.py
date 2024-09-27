bf9b5b14af1df1b383dfd67f936efda25d9e5ae0
aek676 <aek676@inlumine.ual.es>
2024-09-27 22:06:13 +0200

    Pastelink done

--- Pastelink.py
+++ Pastelink.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv, dotenv_values
-import requests, readline
+import requests, readline, json
 
 load_dotenv()
 env = dotenv_values()
@@ -26,43 +26,99 @@ class Pastelink:
         res = requests.post(url, data)
 
         if res.status_code == 200:
-            print(res.json())
+            print(json.dumps(res.json(), indent=2))
             return res.json()['url']
         else:
             print(f"Error to create the paste. Status code: {res.status_code}")
             return res.text
         
-    def edit_paste(self, paste_url):
-        paste = requests.get(f"https://{self.endpoint}/get-paste?url={paste_url}&api_key={self.api_key}").json()
-        title = paste['title']
-        body = paste['body']
+    def get_paste(self, paste_url):
+        url = f"https://{self.endpoint}/get-paste?url={paste_url}&api_key={self.api_key}"
+        res = requests.get(url)
+        return res.json()
+    
+    def get_pastes(self, page, limit, deleted):
+        url = f"https://{self.endpoint}/get-pastes?api_key={self.api_key}&page={page}&limit={limit}&deleted={deleted}"
+        res = requests.get(url)
+        return res.json()
 
-        print(f"Title: {title}")
-        readline.set_startup_hook(lambda: readline.insert_text(body))
-        try:
-            body = input("Add content: ")
-        finally:
-            readline.set_pre_input_hook
-
-        print(body)
+    def edit_body_paste(self, paste_url, body):
         url = f"https://{env['ENDPOINT']}/edit-paste"
         data = {
             'api_key': self.api_key,
             'dry_run': 0,
-            'body': str(body),
+            'body': body,
             'url': paste_url
         }
         
         res = requests.post(url, data)
         res_text_obj = res.json()
-        print(res_text_obj)
+
+        if res.status_code == 200:
+            print(res_text_obj)
+            return(res_text_obj['archive_entry_url'])
+        else:
+            print(f"Error to edit the paste. Status code: {res.status_code}")
+            return res.text
+
+
+    def delete_paste(self, paste_url):
+        url = f"https://{self.endpoint}/delete-paste"
+        data = {
+            'api_key' : self.api_key,
+            'url' : paste_url,
+            'dry_run' : 0
+        }
+        res = requests.post(url, data)
+        print(json.dumps(res.json(), indent=2))
+
+
+    def print_paste(self, paste_url):
+        return print(f"Paste JSON: \n{json.dumps(self.get_paste(paste_url), indent=2)}")
+    
+    def print_pastes(self, page, limit, deleted):
+        return print(f"Pastes JSON: \n{json.dumps(self.get_pastes(page, limit, deleted), indent=2)}")
 
 
 
-api = Pastelink()
+# if __name__ == "__main__":
+#     api = Pastelink()
 
-new_paste_url = 'lbsmqy9w'
-api.edit_paste(new_paste_url)
+#     print("Vas a crear un Pastelink, introduce los siguientes datos:")
+#     title = input("Title: ")
+#     body = input("Body: ")
+#     pasteURL = api.create_paste(title, body)
 
+#     print("Vas a imprimir un pastelink")
+#     pasteURL = input("PasteURL: ")
+#     api.print_paste(pasteURL)
 
+#     print("Vas a imprimir todos los pastelinks que tienes")
+#     page = input("Page: ")
+#     limit = input("Limit: ")
+#     deleted = input("Deleted: ")
+#     api.print_pastes(page, limit, deleted)
 
+#     print("Vas a editar un pastelink")
+#     pasteURL = input("PasteURL: ")
+#     pasteToEdit = api.get_paste(pasteURL)
+#     title = pasteToEdit['title']
+#     body = pasteToEdit['body']
+
+#     print(f"Title: {title}")
+#     readline.set_startup_hook(lambda: readline.insert_text(f"{body}\n"))
+#     try:
+#         body = input("Add content:\n").replace("\\n", "\n")
+#     finally:
+#         readline.set_startup_hook()
+    
+#     api.edit_body_paste(pasteURL, body)
+
+#     print("Comprobamos lo editado")
+#     pasteEdited = api.get_paste(pasteURL)
+#     print(f"Title: {pasteEdited['title']}")
+#     print(f"Body: \n{pasteEdited['body']}")
+
+#     print("Eliminas un paste")
+#     pasteURL = input("PasteURL: ")
+#     api.delete_paste(pasteURL)
